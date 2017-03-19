eeb631b7eeccca3b859c59e001430c842fb97f0b
shirase_aoi <ieee0824@ezweb.ne.jp>
2017-03-19 23:46:18 +0900

    集計する

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+const (
+	token = ""
+	user  = ""
+)
+
+func main() {
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	list := []string{}
+
+	for i := 1; i <= 3; i++ {
+		options := &github.RepositoryListOptions{}
+		options.Page = i
+
+		repos, _, err := client.Repositories.List(ctx, user, options)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		for _, repo := range repos {
+			list = append(list, *repo.Name)
+		}
+	}
+
+	result := make([]int, 24)
+	for _, v := range list {
+		punchcards, _, err := client.Repositories.ListPunchCard(ctx, token, v)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		for _, punchcard := range punchcards {
+			result[*punchcard.Hour] += *punchcard.Commits
+		}
+
+	}
+	for k, v := range result {
+		fmt.Println(k, v)
+	}
+}
